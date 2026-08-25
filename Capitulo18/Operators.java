a05388ef7947b8d12174f4271e9ebd8444fb1fc9
Shary Velasquez <shary.velasquez@campusucc.edu.co>
2026-08-24 23:55:09 -0500

    Add chapter 18 arithmetic operators

---
+++ Capitulo18/Operators.java
@@ -0,0 +1,17 @@
+public class Operators {
+
+    public static void main(String[] args) {
+
+        // Arithmetic operators
+
+        var a = 5;
+        var b = 3;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+    }
+}
