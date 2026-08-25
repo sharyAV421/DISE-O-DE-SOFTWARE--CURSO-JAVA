7eb4bd4ff65ad97e042230496d5872b0c16aa91b
Shary Velasquez <shary.velasquez@campusucc.edu.co>
2026-08-25 00:59:54 -0500

    Add chapter 37 switch statement

---
+++ Capitulo37/Conditionals.java
@@ -0,0 +1,20 @@
+public class Conditionals {
+
+    public static void main(String[] args) {
+
+        // switch
+        var day = 1;
+
+        switch (day) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miércoles");
+                break;
+        }
+    }
+}
