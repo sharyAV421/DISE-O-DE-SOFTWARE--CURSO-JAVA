52a2dfafa2101959e18b0a93a3e13fa0cee2c9ad
Shary Velasquez <shary.velasquez@campusucc.edu.co>
2026-08-25 00:21:37 -0500

    Add chapter 24 strings

---
+++ Capitulo24/Strings.java
@@ -0,0 +1,13 @@
+public class Strings {
+
+    public static void main(String[] args) {
+
+        // String declaration
+
+        String name = "Brais";
+        var surname = new String("Moure");
+
+        System.out.println(name);
+        System.out.println(surname);
+    }
+}
