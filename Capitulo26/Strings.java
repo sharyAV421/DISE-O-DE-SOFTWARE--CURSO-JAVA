30af15dd8e3a7aa81576bd39aceb7867c8281bc3
Shary Velasquez <shary.velasquez@campusucc.edu.co>
2026-08-25 00:26:15 -0500

    Add chapter 26 string length

---
+++ Capitulo26/Strings.java
@@ -0,0 +1,15 @@
+public class Strings {
+
+    public static void main(String[] args) {
+
+        String name = "Brais";
+        var surname = new String("Moure");
+
+        // Concatenation
+        System.out.println(name + " " + surname);
+
+        // Length
+        System.out.println(name.length());
+        System.out.println(surname.length());
+    }
+}
