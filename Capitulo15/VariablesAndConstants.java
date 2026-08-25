1b6a48fb3d822d296ff6b51326991bba294b74dc
Shary Velasquez <shary.velasquez@campusucc.edu.co>
2026-08-24 23:26:46 -0500

    Add chapter 15 variables and constants

---
+++ Capitulo15/VariablesAndConstants.java
@@ -0,0 +1,28 @@
+public class VariablesAndConstants {
+
+    public static void main(String[] args) {
+
+        // Variables
+
+        String name = "Shary";
+        System.out.println(name);
+
+        name = "Sara";
+        System.out.println(name);
+
+        int age = 22;
+        System.out.println(age);
+
+        var email = "Velasquezsara421@gmai.com";
+        System.out.println(email);
+
+        var year = 2026;
+        System.out.println(year);
+
+        // Constants
+
+        final String EMAIL = "Velasquezsara421@gmai.com";
+        System.out.println(EMAIL);
+    }
+}
+
