a67d6571c7b6b01c0e9cd774cf262c9c87e2bd1f
Shary Velasquez <shary.velasquez@campusucc.edu.co>
2026-08-25 00:44:18 -0500

    Add chapter 35 if statement

---
+++ Capitulo35/Conditionals.java
@@ -0,0 +1,11 @@
+public class Conditionals {
+
+    public static void main(String[] args) {
+
+        var age = 16;
+
+        if (age >= 18) {
+            System.out.println("El usuario es mayor de edad");
+        }
+    }
+}
