a6ca60f79bae0b1087b6c4ddac430494bba66753
Shary Velasquez <shary.velasquez@campusucc.edu.co>
2026-08-25 00:30:16 -0500

    Add chapter 29 string case conversion

---
+++ Capitulo29/Strings.java
@@ -0,0 +1,28 @@
+public class Strings {
+
+    public static void main(String[] args) {
+
+        String name = "Brais";
+        var surname = new String("Moure");
+
+        // Concatenation
+        System.out.println(name + " " + surname);
+
+        // Length
+        System.out.println(name.length());
+        System.out.println(surname.length());
+
+        // charAt
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+
+        // substring
+        System.out.println(name.substring(1));
+        System.out.println(name.substring(1, 4));
+
+        // Uppercase and lowercase
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
+
