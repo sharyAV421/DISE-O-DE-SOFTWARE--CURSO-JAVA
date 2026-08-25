abc1c53a00ccb65b766b3c44644f60028874500b
Shary Velasquez <shary.velasquez@campusucc.edu.co>
2026-08-25 00:24:46 -0500

    Add chapter 25 string concatenation

---
+++ Capitulo25/Concatenación.java
@@ -0,0 +1,12 @@
+public class Strings {
+
+    public static void main(String[] args) {
+
+        String name = "Brais";
+        var surname = new String("Moure");
+
+        // Concatenation
+        System.out.println(name + " " + surname);
+    }
+}
+
