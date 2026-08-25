6e240eaaeccbb6e6a3d4d59481d37afd44ddde46
Shary Velasquez <shary.velasquez@campusucc.edu.co>
2026-08-25 00:15:43 -0500

    Add chapter 22 unary operators

---
+++ Capitulo22/UnaryOperators.java
@@ -0,0 +1,21 @@
+public class UnaryOperators {
+
+    public static void main(String[] args) {
+
+        var b = 4;
+
+        // Unary operators
+
+        System.out.println(+b);
+        System.out.println(-b);
+
+        System.out.println(++b);
+        System.out.println(b++);
+
+        System.out.println(b);
+
+        System.out.println(--b);
+        System.out.println(b--);
+    }
+}
+
