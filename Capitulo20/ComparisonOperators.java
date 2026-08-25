778ea8dea12e424be2090c5663f686b79f33b4eb
Shary Velasquez <shary.velasquez@campusucc.edu.co>
2026-08-25 00:07:09 -0500

    Add chapter 20 comparison operators

---
+++ Capitulo20/ComparisonOperators.java
@@ -0,0 +1,18 @@
+public class ComparisonOperators {
+
+    public static void main(String[] args) {
+
+        var a = 5;
+        var b = 3;
+
+        // Comparison operators
+
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+
+    }
+}
