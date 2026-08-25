0b936bffa96f66a788551f5abdfdbbc351389137
Shary Velasquez <shary.velasquez@campusucc.edu.co>
2026-08-24 23:40:20 -0500

    Add chapter 16 data types

---
+++ Capitulo16/DataTypes.java
@@ -0,0 +1,28 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        
+
+        int myInt = 37;
+        System.out.println(myInt);
+
+        double myDouble = 1.77;
+        System.out.println(myDouble);
+
+     
+
+        char myChar = 'a';
+        System.out.println(myChar);
+
+        boolean myBoolean = true;
+        myBoolean = false;
+        System.out.println(myBoolean);
+
+        String myString = "Hola, Java";
+        System.out.println(myString);
+
+      
+        System.out.println(myString.getClass().getSimpleName());
+    }
+}
