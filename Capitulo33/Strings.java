10795c808ce5fb0e8694fd22fc800caf5f36215c
Shary Velasquez <shary.velasquez@campusucc.edu.co>
2026-08-25 00:38:11 -0500

    Add chapter 33 string format

---
+++ Capitulo33/Strings.java
@@ -0,0 +1,45 @@
+public class Strings {
+
+    public static void main(String[] args) {
+
+        String name = "Brais";
+        var surname = new String("Moure");
+
+        // Concatenation
+        System.out.println(name + " " + surname);
+
+        // Length
+        System.out.println(name.length());
+        System.out.println(surname.length());
+
+        // charAt
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+
+        // substring
+        System.out.println(name.substring(1));
+        System.out.println(name.substring(1, 4));
+
+        // Uppercase and lowercase
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+
+        // Contains
+        System.out.println(name.contains("B"));
+        System.out.println(name.contains("z"));
+
+        // Equals
+        System.out.println(name.equals("Brais"));
+        System.out.println(name.equals("brais"));
+
+        // Trim
+        String text = "  Hello Java  ";
+        System.out.println(text.trim());
+
+        // Replace
+        System.out.println(name.replace("a", "o"));
+
+        // Format
+        System.out.println(String.format("Hello, %s %s", name, surname));
+    }
+}
